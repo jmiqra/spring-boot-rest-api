fd6745b284c24da0d89372e6cc5ee7b78d020fcd
Asraf Uddin Ahmed <asraf@dginnovationlab.com>
2018-05-13 12:23:10 +0600

    Lombok annotation add into ResponseDto

--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/core/dtos/response/UserResponseDto.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/core/dtos/response/UserResponseDto.java
@@ -1,22 +1,9 @@
 package com.asraf.core.dtos.response;
 
+import lombok.Data;
+
+@Data
 public class UserResponseDto {
 	private String email;
 	private String name;
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
