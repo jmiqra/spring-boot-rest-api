6aa16aa0105b9b757ee0fa7e35ee050f989f7be9
Asraf Uddin Ahmed <asraf@dginnovationlab.com>
2018-07-03 16:41:01 +0600

    Prepare for deploy in Tomcat servlet container

--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/SpringBootHibernateDatajpaApplication.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/SpringBootHibernateDatajpaApplication.java
@@ -2,9 +2,10 @@ package com.asraf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootHibernateDatajpaApplication {
+public class SpringBootHibernateDatajpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHibernateDatajpaApplication.class, args);
