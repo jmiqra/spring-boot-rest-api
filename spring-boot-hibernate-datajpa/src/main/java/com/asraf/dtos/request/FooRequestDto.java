559892c026a7946887c716ac634e8f5a46ffb6f2
Jurdana Iqrah <iqrah@dginnovationlab.com>
2018-07-26 15:43:02 +0600

    IsEqualConstraint validator added

--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/dtos/request/FooRequestDto.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/dtos/request/FooRequestDto.java
@@ -1,7 +1,6 @@
 package com.asraf.dtos.request;
 
-import com.asraf.validators.RequiredIfExistsConstraint;
-import com.asraf.validators.RequiredIfNotExistsConstraint;
+import com.asraf.validators.IsEqualConstraint;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,18 +9,19 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-//@RequiredIfExistsConstraint.List({
-//		@RequiredIfExistsConstraint(baseField = "field1", dependentField = "field2", message = "1st annonation error"),
-//		// @RequiredIfExistsConstraint(baseField = "field2", dependentField = "field1")
-//})
-@RequiredIfNotExistsConstraint.List({
-	@RequiredIfNotExistsConstraint(baseField = "field1", dependentField = "field2", message = "1st annonation error"),
-	// @RequiredIfExistsConstraint(baseField = "field2", dependentField = "field1")
+// @RequiredIfExistsConstraint.List({
+// @RequiredIfExistsConstraint(baseField = "field1", dependentField = "field2",
+// message = "1st annonation error"),
+// // @RequiredIfExistsConstraint(baseField = "field2", dependentField =
+// "field1")
+// })
+@IsEqualConstraint.List({
+		@IsEqualConstraint(baseField = "field1", dependentField = "field2", message = "Not Equal error"),
 })
 public class FooRequestDto extends BaseRequestDto {
 
-	private String field1;
-	private String field2;
+	private float field1;
+	private float field2;
 	// private String[] list;
 
 }
---
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/validators/IsEqualConstraint.java
@@ -0,0 +1,36 @@
+package com.asraf.validators;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {IsEqualValidator.class})
+@Documented
+public @interface IsEqualConstraint {
+ 
+    String message() default "Invalid matching";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+ 
+    String baseField();
+ 
+    String dependentField();
+    
+    //String[] stringList();
+    
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List
+    {
+    	IsEqualConstraint[] value();
+    }
+    
+}
