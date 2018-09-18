0c6eb536dcc488f5fbbd38f1611077e72da3b841
Asraf Uddin Ahmed <asraf@dginnovationlab.com>
2018-09-18 13:43:40 +0600

    Content negotiation support enabled for JSON, HAL+JSON and XML

--- spring-boot-hibernate-datajpa/src/main/java/com/asraf/WebConfig.java
+++ spring-boot-hibernate-datajpa/src/main/java/com/asraf/WebConfig.java
@@ -1,6 +1,9 @@
 package com.asraf;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,4 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("*");
 	}
+	
+	@Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+    configurer.favorPathExtension(true)
+            .favorParameter(true)
+            .defaultContentType(MediaTypes.HAL_JSON)
+            .mediaType("json", MediaType.APPLICATION_JSON)
+            .mediaType("xml", MediaType.APPLICATION_XML)
+            .mediaType("hal_json", MediaTypes.HAL_JSON);
+    }
+	
 }
